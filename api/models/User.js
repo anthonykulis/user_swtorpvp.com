6562661e9a19ba491dfddb9d1fb8798d96ae435b
anthonykulis <anthony.kulis@gmail.com>
2016-01-11 09:12:41 -0600

    getting postgres working

--- api/models/User.js
+++ api/models/User.js
@@ -32,7 +32,7 @@ module.exports = {
     toJSON: function() {
         var obj = this.toObject();
         delete obj.password;
-        delete user.password_confirmation;
+        delete obj.password_confirmation;
         return obj;
     }
   },
