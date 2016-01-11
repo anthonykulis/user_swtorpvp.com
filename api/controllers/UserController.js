0391b5e3fe5442c540281da794cdf915715ebeeb
anthonykulis <anthony.kulis@gmail.com>
2016-01-11 06:06:27 -0600

    allowing create user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,17 +8,17 @@
 module.exports = {
 	find: function(req, res){
     res.json({works: true});
-  }
+  },
 
   create: function(req,res){
-    User.create(req.body).exec(err, user){
+    User.create(req.body).exec(function(err, user){
       if(err){
         res.json(err);
       }
       else{
         res.json(user);
       }
-    }
+    });
   }
 };
 
