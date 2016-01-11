e1c29dce340094d8b01e72882c699eb190a2fa61
anthonykulis <anthony.kulis@gmail.com>
2016-01-11 09:15:20 -0600

    building migration for users

---
+++ db/migrations/20160111091418_drop_create_users.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  
+  knex.schema.dropTable('users');
+  knex.schema.createTable('user', function(table){
+    table.increments();
+    table.timestamps();
+    table.string('email');
+    table.string('password');
+    table.boolean('emails_allowed');
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  
+};
