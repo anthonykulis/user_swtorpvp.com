a325c905d266d20fc0df5e44ec155487703babdb
anthonykulis <anthony.kulis@gmail.com>
2016-01-11 07:18:15 -0600

    fixes typo on attributes/middleware

--- api/models/User.js
+++ api/models/User.js
@@ -18,24 +18,25 @@ module.exports = {
       required: true,
       type: 'string',
       minLength: 8
-    },
-    beforeCreate: function(user, cb) {
-      if(user.password !== user.password_confirmation){
-        // todo: needs to go to error parser/builder
-        cb({error: 'Password does not match password confirmation'})
-      }
-      bcrypt.genSalt(16, function(err, salt){
-        bcrypt.hash(user.password, salt, function(err, hash){
-          if(err){
-            cb(err);
-          }
-          else {
-            user.password = hash;
-            cb();
-          }
-        });
-      });
     }
+  },
+  beforeCreate: function(user, cb) {
+    if(user.password !== user.password_confirmation){
+      // todo: needs to go to error parser/builder
+      cb({error: 'Password does not match password confirmation'})
+    }
+    bcrypt.genSalt(16, function(err, salt){
+      bcrypt.hash(user.password, salt, function(err, hash){
+        if(err){
+          cb(err);
+        }
+        else {
+          user.password = hash;
+          cb();
+        }
+      });
+    });
   }
+  
 };
 
