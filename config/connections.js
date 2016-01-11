202bec53e41e4d03b56e08934e45844933446da4
anthonykulis <anthony.kulis@gmail.com>
2016-01-11 07:14:02 -0600

    setting up to use postgres

--- config/connections.js
+++ config/connections.js
@@ -80,7 +80,12 @@ module.exports.connections = {
     user: 'YOUR_POSTGRES_USER',
     password: 'YOUR_POSTGRES_PASSWORD',
     database: 'YOUR_POSTGRES_DB'
-  }
+  },
+
+  productionPostgresServer: {
+    adapter: 'sails-postgresql',
+    url: process.env.DATABASE_URL
+  },
 
 
   /***************************************************************************
