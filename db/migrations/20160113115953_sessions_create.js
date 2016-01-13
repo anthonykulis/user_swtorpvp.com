6297c16f3d45122aa6385d9ae5c5820715900f59
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 12:18:09 -0600

    again trying migration

--- db/migrations/20160113115953_sessions_create.js
+++ db/migrations/20160113115953_sessions_create.js
@@ -1,16 +1,14 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  console.log('starting create of session table');
   knex.schema.createTable('session', function(table){
-    console.log('in table');
     table.increments();
     table.timestamps();
-    table.integer('user').index().inTable('user').references('id');
+    table.bigInteger('user').index().inTable('user').references('id');
     table.boolean('logged_out');
   });  
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.dropTable('session');  
 };
