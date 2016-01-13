b5e29069b467f3ca6ef1f5193644ddac3b2808d1
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 15:54:02 -0600

    turns off db connection

--- config/env/production.js
+++ config/env/production.js
@@ -35,9 +35,9 @@ module.exports = {
   //   level: "silent"
   // }
 
-  models: {
-    connection: 'productionPostgresServer',
-    migrate: 'alter'
-  }
+  // models: {
+  //   connection: 'productionPostgresServer',
+  //   migrate: 'alter'
+  // }
 
 };
