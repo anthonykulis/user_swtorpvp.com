ee15854c5f5fe03c348abd70920f07b5bf3252bb
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 15:18:30 -0600

    more backtracking

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -1,55 +1,55 @@
-/**
- * SessionController
- *
- * @description :: Server-side logic for managing Sessions
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+// /**
+//  * SessionController
+//  *
+//  * @description :: Server-side logic for managing Sessions
+//  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+//  */
 
-var passport = require('passport');
+// var passport = require('passport');
 
-module.exports = {
+// module.exports = {
 	
-  // log the user in
-  create: function(req, res) {
-    console.log('creating session');
-    passport.authenticate('local', function(err, user, info){
-      console.log('on auth', user, info);
-      if(err){ res.json(500, err); }
-      else if(!user){ res.json(422, {message: 'Bad Email/Password'})}
-      else 
-        req.logIn(user, function(err){
-          if(err){ res.json(500, err);}
-          else 
-            Session.findOrCreate({user: user.id, active: true}).exec(function(err, session){
-              if(err){ res.json(500, err); }
-              res.json(session);
-            });
+//   // log the user in
+//   create: function(req, res) {
+//     console.log('creating session');
+//     passport.authenticate('local', function(err, user, info){
+//       console.log('on auth', user, info);
+//       if(err){ res.json(500, err); }
+//       else if(!user){ res.json(422, {message: 'Bad Email/Password'})}
+//       else 
+//         req.logIn(user, function(err){
+//           if(err){ res.json(500, err);}
+//           else 
+//             Session.findOrCreate({user: user.id, active: true}).exec(function(err, session){
+//               if(err){ res.json(500, err); }
+//               res.json(session);
+//             });
             
-        });
-    })(req, res);
-  },
+//         });
+//     })(req, res);
+//   },
 
-  destroy: function(req, res){
+//   destroy: function(req, res){
 
-    // okay, user has to be logged in - I AM NOT 100% sure on this - might need update
-    if(!(req.session && req.session.passport && req.session.passport.user))
-      return res.json(401, {});
+//     // okay, user has to be logged in - I AM NOT 100% sure on this - might need update
+//     if(!(req.session && req.session.passport && req.session.passport.user))
+//       return res.json(401, {});
 
 
-    Session.update({id: req.params.id, user: req.session.passport.user}, {active: false}).exec(function(err, session){
-      if(err){ res.json(500, err); }
-      else
-        req.logOut();
-        return res.json({});
-    });
-  },
+//     Session.update({id: req.params.id, user: req.session.passport.user}, {active: false}).exec(function(err, session){
+//       if(err){ res.json(500, err); }
+//       else
+//         req.logOut();
+//         return res.json({});
+//     });
+//   },
 
-  // not meant to be public.... so will eventually need to be authed and cross-referenced aganst role
-  find: function(req, res){
-    Session.find().populate('user').exec(function(err, sessions){
-      if(err) res.json(500, err);
-      else res.json(sessions);
-    });
-  }
-};
+//   // not meant to be public.... so will eventually need to be authed and cross-referenced aganst role
+//   find: function(req, res){
+//     Session.find().populate('user').exec(function(err, sessions){
+//       if(err) res.json(500, err);
+//       else res.json(sessions);
+//     });
+//   }
+// };
 
