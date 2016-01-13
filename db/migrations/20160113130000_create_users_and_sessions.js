793a550a043244a599e1bb813a0edde82157bcac
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 13:00:10 -0600

    again

---
+++ db/migrations/20160113130000_create_users_and_sessions.js
@@ -0,0 +1,23 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  knex.schema.createTable('user', function(table){
+    table.increments();
+    // table.timestamps();
+    table.string('email');
+    table.string('password');
+    table.boolean('emails_allowed');
+  });
+
+  // knex.schema.createTable('session', function(table){
+  //   table.increments();
+  //   table.timestamps();
+  //   // table.bigInteger('user_id').index().inTable('user').references('id');
+  //   table.boolean('logged_in');
+  // });  
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('session'); 
+  knex.schema.dropTable('user');
+};
