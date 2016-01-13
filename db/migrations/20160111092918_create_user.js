5652bd238bb9bdd7ebcd3f2756a034d8398a2b8a
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 12:47:53 -0600

    trying all in one

--- db/migrations/20160111092918_create_user.js
+++ db/migrations/20160111092918_create_user.js
@@ -1,15 +1,23 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  knex.schema.withSchema('public').createTable('user', function(table){
+  knex.schema.createTable('user', function(table){
     table.bigIncrements();
     table.timestamps();
     table.string('email');
     table.string('password');
     table.boolean('emails_allowed');
   });
+
+  knex.schema.createTable('session', function(table){
+    table.bigIncrements();
+    table.timestamps();
+    table.bigInteger('user_id').index().inTable('user').references('id');
+    table.boolean('logged_in');
+  });  
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.dropTable('session'); 
+  knex.schema.dropTable('user');
 };
