1aa0d8524c8167f72f64452e4d62f270a8c84d39
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 11:46:35 -0600

    testing migration

--- db/migrations/20160113114607_sessions_create.js
+++ db/migrations/20160113114607_sessions_create.js
@@ -4,9 +4,9 @@ exports.up = function(knex, Promise) {
   knex.schema.createTable('session', function(table){
     table.increments();
     table.timestamps();
-    table.integer('user').index().inTable('user').references('id');
+    // table.integer('user').index().inTable('user').references('id');
     table.string('logged_out');
-  });
+  });  
 };
 
 exports.down = function(knex, Promise) {
