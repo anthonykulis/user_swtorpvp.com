c296c20c1b66739c9b445278900657d0a5c4b20f
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 13:51:25 -0600

    retiring

---
+++ db/migrations/20160113134412_create_user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  knew.schema.createTable('user', function(table){
+    table.bigIncrements();
+    table.timestamps();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.boolean('emails_allowed').defaultTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  
+};
