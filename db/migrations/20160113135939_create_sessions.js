4438c96cb96518e8eedaa2e74d5295da3a88294e
anthonykulis <anthony.kulis@gmail.com>
2016-01-13 15:14:59 -0600

    more backtracking

--- db/migrations/20160113135939_create_sessions.js
+++ db/migrations/20160113135939_create_sessions.js
@@ -1,14 +1,14 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('session', function(table){
-    table.bigIncrements();
-    table.timestamps();
-    table.bigInteger('user').references('id').inTable('user');
-    table.boolean('active').defaultTo(true);
-  });
+  // return knex.schema.createTable('session', function(table){
+  //   table.bigIncrements();
+  //   table.timestamps();
+  //   table.bigInteger('user').references('id').inTable('user');
+  //   table.boolean('active').defaultTo(true);
+  // });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('session');
+  // return knex.schema.dropTable('session');
 };
