0bed5d21dac045e9944d02a994962e9c8c1603ee
anthonykulis <anthony.kulis@gmail.com>
2016-01-26 13:02:06 -0600

    have working, now connecting reds

--- config/session.js
+++ config/session.js
@@ -42,7 +42,7 @@ module.exports.session = {
   ***************************************************************************/
 
 
-  // adapter: 'redis',
+  adapter: 'redis',
   // host: process.env.REDIS_HOST,
   // port: process.env.REDIS_PORT,
   // db: process.env.REDIS_DB,
